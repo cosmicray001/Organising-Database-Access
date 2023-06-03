aeaee2ee57998ae178eaa11dea7694fa028b4a0d
cosmicray001 <samiulislambracu@gmail.com>
2023-06-04 01:40:19 +0600

    add: Using a global variable

--- main.go
+++ main.go
@@ -1,19 +1,49 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"github.com/cosmicray001/Organising-Database-Access/models"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	var err error
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	// Initialize the sql.DB connection pool and assign it to the models.DB
+	// global variable.
+	models.DB, err = sql.Open("postgres", os.Getenv("DB_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		_, _ = w.Write([]byte("Hello word!"))
 	})
+	mux.HandleFunc("/books", booksIndex)
 	log.Println("Listening and serving on port: 8000")
-	err := http.ListenAndServe(":8000", mux)
+	err = http.ListenAndServe(":8000", mux)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func booksIndex(w http.ResponseWriter, r *http.Request) {
+	bks, err := models.AllBooks()
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	for _, bk := range bks {
+		fmt.Fprintf(w, "%s, %s, %s, £%.2f\n", bk.Isbn, bk.Title, bk.Author, bk.Price)
 	}
 }
--- models/models.go
+++ models/models.go
@@ -1,1 +1,33 @@
 package models
+
+import "database/sql"
+
+var DB *sql.DB
+
+type Book struct {
+	Isbn   string
+	Title  string
+	Author string
+	Price  float32
+}
+
+func AllBooks() ([]Book, error) {
+	rows, err := DB.Query("SELECT * FROM books")
+	defer rows.Close()
+	if err != nil {
+		return nil, err
+	}
+	var bks []Book
+	for rows.Next() {
+		var bk Book
+		err := rows.Scan(&bk.Isbn, &bk.Title, &bk.Author, &bk.Price)
+		if err != nil {
+			return nil, err
+		}
+		bks = append(bks, bk)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return bks, nil
+}
